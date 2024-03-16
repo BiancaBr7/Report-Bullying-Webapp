40fc6ba2449fa43574fd9e643438ec7a92bd2a2b
Bianca Bai <110609439+BiancaBr7@users.noreply.github.com>
2024-03-16 13:19:40 -0400

    changes

--- reportBullying/settings.py
+++ reportBullying/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'reportBullying.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres.shvsxrmzdoyolilvbuwa',
+        'PASSWORD': '&79UEL.GAaJ-rUr',
+        'HOST':'aws-0-ca-central-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
 

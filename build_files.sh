46ada4eb0e9e993ef06e42684989b80eda0dc27f
Bianca Bai <110609439+BiancaBr7@users.noreply.github.com>
2024-03-22 18:53:21 -0400

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
 npm install nodejs
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
